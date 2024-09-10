d97f9082b1761ba9660ec696551056b21b254ea6
Yasuaki Gohko <ygohko@gmail.com>
2024-09-11 08:06:18 +0900

    Updated test codes.

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,18 @@ struct Args {
     /// Path to a file or directory that you need
     #[arg(short, long)]
     path: Option<String>,
+    /// Revision count to keep
+    #[arg(short, long)]
+    count: Option<i32>,
 }
 
 #[derive(Subcommand)]
 enum Commands {
+    Init,
     Commit,
     Log,
+    Get,
+    Forget,
 }
 
 fn main() {
@@ -25,8 +31,11 @@ fn main() {
 
     let args = Args::parse();
     let command = match args.command.unwrap() {
+        Commands::Init => "init",
         Commands::Commit => "commit",
         Commands::Log => "log",
+        Commands::Get => "get",
+        Commands::Forget => "forget",
     };
     println!("{}", command);
     if args.revision.is_some() {
@@ -34,5 +43,8 @@ fn main() {
     }
     if args.path.is_some() {
         println!("{}", args.path.unwrap());
-    }    
+    }
+    if args.count.is_some() {
+        println!("{}", args.count.unwrap());  
+    }
 }
