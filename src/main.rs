915a835777ae65faef731a954f8954c5c37fac03
Yasuaki Gohko <ygohko@gmail.com>
2024-09-10 12:47:36 +0900

    Added test for subcommands.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 use clap::Parser;
+use clap::Subcommand;
 
 #[derive(Parser)]
 struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
     #[arg(long)]
     pos: String,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Commit,
+    Log,
 }
 
 fn main() {
     println!("Hello, world!");
 
     let args = Args::parse();
+    let command = match args.command.unwrap() {
+        Commands::Commit => "commit",
+        Commands::Log => "log",
+    };
+    println!("{}", command);
     println!("{}", args.pos);
 }
